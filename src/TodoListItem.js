65650eb78639e3f24707cc9bcdeb1274dfbcec34
VanessaSR <103793183+vsrodriguez3@users.noreply.github.com>
2023-11-24 19:57:15 -0500

    Add InputWithLabel component, auto-focus, and button to remove task.

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function TodoListItem({ title }) {
-    return (
-        <li>
-          {title}
-        </li>
-    );
+function TodoListItem({ title, id, onRemoveTodo }) {
+
+  return (
+      <li>
+        {title}
+        <button 
+          type="button" 
+          onClick={() => onRemoveTodo(id)}
+        >
+          Remove
+        </button>
+      </li>
+  );
 };
 
 export default TodoListItem;
